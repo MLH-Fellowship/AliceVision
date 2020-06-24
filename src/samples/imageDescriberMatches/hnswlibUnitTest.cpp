30a3788c1d26d8df3f5bf01a1d3016b1ac9740fd
Amir Yalamov <amir.yalamov@gmail.com>
2020-06-24 18:58:30 -0400

    create unit test for hnswlib

---
+++ src/samples/imageDescriberMatches/hnswlibUnitTest.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Amir Yalamov (https://github.com/AmirYalamov) on 2020-06-23.
+//
+
+/*
+ * This is a unit test for the hnswlib library (https://github.com/nmslib/hnswlib)
+ *
+ * It will test the different classe/functions that are needed for hnswlib
+ */
+
+// Test the the building of the matching structure
+
+
+
+// test the nearest neighbour searches
+
+
